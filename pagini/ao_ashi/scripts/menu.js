5542de05e79f5e30201ec9f98d7a6d9467db4a30
iTsMarian2893 <marianghe2003@gmail.com>
2025-02-12 22:10:19 +0200

    Delete pagini/ao_ashi directory

--- pagini/ao_ashi/scripts/menu.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    var menuContainer = document.querySelector('.menu-container');
-    document.body.classList.toggle('menu-open'); // Adaugă/elimină clasa "menu-open" la body pentru a deschide/închide meniul
-  }
